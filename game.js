2fa1dc30081fb40b097f2e887772415c30341a3d
Dary <dady281100@gmail.com>
2023-03-06 00:03:12 -0600

    First assignment added

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
 // Let's start
+
+// Firs Assignment
+// Write a JavaScript program to find the diagonal of a square where the length of each
+// side is 9.
+function result(side=9, division=2){
+    let diagonal = side*4/division; 
+    window.alert(diagonal);    
+}
+
+result();

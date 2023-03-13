884e57cab32f5e030160d3cad3883d28ca4a011d
Israel Aguilar <dady281100@gmail.com>
2023-03-12 23:55:52 -0500

    Update game.js

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,19 @@
 // Regular expressions added
 const rockRegex = /^rock$/i,
-paperRegex = /^paper$/i,
-exitRegex = /^exit$/i,
-scissorsRegex = /^scissors$/i;
+    paperRegex = /^paper$/i,
+    exitRegex = /^exit$/i,
+    scissorsRegex = /^scissors$/i;
+
+player_game = (input) => {
+    if (paperRegex.test(input)) {
+        return paperGame
+    } else if (rockRegex.test(input)) {
+        return rockGame
+    } else if (scissorsRegex.test(input)) {
+        return scissorsGame
+    } else if (exitRegex.test(input)){
+        return 'exit'
+    } else {
+        return false
+    };
+},
